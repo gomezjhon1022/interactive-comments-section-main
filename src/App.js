ffdbc13ebf47d2229be7f16bd82d672bb7479cc0
Jhon Gomez <jhonjairogomez92@gmail.com>
2023-08-23 21:01:10 +0200

    Add: Initial layout showcasing basic comment structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import jsonData from "./data/data.json";
+
+
 
 function App() {
+  const [data,setData]= useState();
+  const importData = () => {
+    setData(jsonData);
+  }
+
+  useEffect(()=>{
+    importData();
+  }, [])
+
+  useEffect(() => {
+    console.log("data", data);
+  }, [data])
+
   return (
     <div className="App">
+      {data && data.comments && data.comments.map((element, index)=>(
+        <div>
+          <div className='content' key={index}>{element.content}</div>
+          <div className='createdAt'>{element.createdAt}</div>
+          <div>
+            <img className='userImage' src={element.user.image.png}/>
+            <div className='username'>{element.user.username}</div>
+          </div>
+          <div className='score'>{element.score}</div>
+          <div className='reply'><span>R</span> Reply</div>
+        </div>
+              )
+      )
+      }
     </div>
   );
 }
