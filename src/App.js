b76bffb0245835fc9af678eba1e7343489ac9099
Jhon Gomez <jhonjairogomez92@gmail.com>
2023-08-23 23:15:26 +0200

    Add: Styles for mobile view

--- src/App.js
+++ src/App.js
@@ -20,20 +20,22 @@ function App() {
 
   return (
     <div className="App">
-      {data && data.comments && data.comments.map((element, index)=>(
-        <div>
-          <div className='content' key={index}>{element.content}</div>
-          <div className='createdAt'>{element.createdAt}</div>
-          <div>
-            <img className='userImage' src={element.user.image.png}/>
-            <div className='username'>{element.user.username}</div>
+      <main>
+        {data && data.comments && data.comments.map((element, index)=>(
+          <div className='comment__container' key={index}>
+            <div className='contentComment' >{element.content}</div>
+            <div className='createdAt'>{element.createdAt}</div>
+            <div className='user'>
+              <img className='userImage' src="https://images.pexels.com/photos/16628785/pexels-photo-16628785/free-photo-of-moda-amor-mujer-oscuro.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt='avatar'/>
+              <div className='username'>{element.user.username}</div>
+            </div>
+            <div className='score'><span className='plus'></span>{element.score}<span className='minus'></span></div>
+            <div className='reply'><span className='replyIcon'></span> Reply</div>
           </div>
-          <div className='score'>{element.score}</div>
-          <div className='reply'><span>R</span> Reply</div>
-        </div>
-              )
-      )
-      }
+                )
+        )
+        }
+      </main>
     </div>
   );
 }
