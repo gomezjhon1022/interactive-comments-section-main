00996e7669c15c77b2389f607fcb260da533ca2c
Jhon Gomez <jhonjairogomez92@gmail.com>
2023-08-27 17:56:54 +0200

    Refactor: Consolidate score handling into a single function

--- src/App.js
+++ src/App.js
@@ -6,95 +6,103 @@ import jsonData from "./data/data.json";
 
 function App() {
   const [data,setData]= useState(jsonData);
-  const [addComment, setAddComment]= useState(false);
+  const [addComment, setAddComment]= useState();
     console.log("data", data);
 
   const handleReply = ()=> {
-    setAddComment(!addComment);
+    // setAddComment(!addComment);
   }
 
-  const handleplus = (comment) => {
-    const updatedData = data.comments.map((element) => {
-      if (element.id===comment.id) {
+  const handleScore = (comment,operation) => {
+    let value;
+    if (operation==='plus') {
+      value=+1;
+    } else {
+      value=-1;
+    }
+
+
+    const updatedData = data.comments.map((elementData) => {
+      if (elementData.id===comment.id) {
         return {
-          ...element,
-          score: element.score + 1
+          ...elementData,
+          score: elementData.score + value
         };
       }
-      let updateReplies= element.replies;
-      if (element.replies.length > 0) {
-          updateReplies = element.replies.map((rep) => {
+      let updateReplies= elementData.replies;
+      if (elementData.replies.length > 0) {
+          updateReplies = elementData.replies.map((rep) => {
           if (rep.id===comment.id) {
             return {
               ...rep,
-              score: rep.score +1
+              score: rep.score + value
             };
           }
           return rep;
           })
       }
       return {
-        ...element,
+        ...elementData,
         replies: updateReplies
       };
     });
     setData({...data,  comments:updatedData})
   }
 
-  const handleminus = (comment) => {
-    const updatedData = data.comments.map((element) => {
-      if (element.id===comment.id) {
-        return {
-          ...element,
-          score: element.score - 1
-        };
-      }
-      let updateReplies= element.replies;
-      if (element.replies.length > 0) {
-          updateReplies = element.replies.map((rep) => {
-          if (rep.id===comment.id) {
-            return {
-              ...rep,
-              score: rep.score -1
-            };
-          }
-          return rep;
-          })
-      }
-      return {...element,
-        replies: updateReplies
-      }
-    });
-    setData({...data,  comments:updatedData});
-  }
+  // const handleminus = (comment) => {
+  //   const updatedData = data.comments.map((elementData) => {
+  //     if (elementData.id===comment.id) {
+  //       return {
+  //         ...elementData,
+  //         score: elementData.score - 1
+  //       };
+  //     }
+  //     let updateReplies= elementData.replies;
+  //     if (elementData.replies.length > 0) {
+  //         updateReplies = elementData.replies.map((rep) => {
+  //         if (rep.id===comment.id) {
+  //           return {
+  //             ...rep,
+  //             score: rep.score -1
+  //           };
+  //         }
+  //         return rep;
+  //         })
+  //     }
+  //     return {...elementData,
+  //       replies: updateReplies
+  //     }
+  //   });
+  //   setData({...data,  comments:updatedData});
+  // }
 
   return (
     <div className="App">
       <main>
-        {data && data.comments && data.comments.map((element)=>(
-          <div key={element.id}>
+        {data && data.comments && data.comments.map((commentsItem)=>(
+          <div key={commentsItem.id}>
             <div className='comment__container' >
-              <div className='contentComment' >{element.content}</div>
-              <div className='createdAt'>{element.createdAt}</div>
+              <div className='contentComment' >{commentsItem.content}</div>
+              <div className='createdAt'>{commentsItem.createdAt}</div>
               <div className='user'>
-                <img className='userImage' src={element.user.image.webp} alt='avatar'/>
-                <div className='username'>{element.user.username}</div>
+                <img className='userImage' src={commentsItem.user.image.webp} alt='avatar'/>
+                <div className='username'>{commentsItem.user.username}</div>
               </div>
-              <div className='score'><span className='plus' onClick={()=>handleplus(element)}></span>{element.score}<span className='minus' onClick={()=>handleminus(element)} ></span></div>
-              <div className='reply' onClick={()=>handleReply(element)}><span className='replyIcon'></span> Reply</div>
+              <div className='score'><span className='plus' onClick={()=>handleScore(commentsItem, 'plus')}></span>{commentsItem.score}<span className='minus' onClick={()=>handleScore(commentsItem,'minus')} ></span></div>
+              <div className='reply' onClick={()=>handleReply(commentsItem)}><span className='replyIcon'></span> Reply</div>
             </div>
             {addComment&& (<div>aca se agrega el comentario </div>)}
             <div className='replies__container'>
-              {element.replies.map((item, index) => (
+              {commentsItem.replies.map((repliesItem, index) => (
                 <div className='comment__container' key={index}>
-                <div className='contentComment' >{item.content}</div>
-                <div className='createdAt'>{item.createdAt}</div>
+                <div className='contentComment' >{repliesItem.content}</div>
+                <div className='createdAt'>{repliesItem.createdAt}</div>
                 <div className='user'>
-                  <img className='userImage' src={item.user.image.webp} alt='avatar'/>
-                  <div className='username'>{item.user.username}</div>
+                  <img className='userImage' src={repliesItem.user.image.webp} alt='avatar'/>
+                  <div className='username'>{repliesItem.user.username}</div>
                 </div>
-                <div className='score'><span className='plus' onClick={()=>handleplus(item)}></span>{item.score}<span className='minus' onClick={()=>handleminus(item)}></span></div>
-                <div className='reply' onClick={()=>handleReply(item)}><span className='replyIcon'></span> Reply</div>
+                <div className='score'><span className='plus' onClick={()=>handleScore(repliesItem, 'plus')}></span>{repliesItem.score}<span className='minus' onClick={()=>handleScore(repliesItem,'minus')}></span></div>
+                <div className='reply' onClick={()=>handleReply(repliesItem)}><span className='replyIcon'></span> Reply</div>
                 </div>
               ))
               }
